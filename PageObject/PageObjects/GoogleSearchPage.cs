e93a1c7c2ba94ba5267fb84ef2738b7dc3e7609d
Vlad Poltorak <vlad.smila@gmail.com>
2018-04-24 16:16:58 +0300

    Add project files.

---
+++ PageObject/PageObjects/GoogleSearchPage.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObject.PageObjects
+{
+    class GoogleSearchPage
+    {
+        private IWebDriver driver;
+
+        public GoogleSearchPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "q")]
+        private IWebElement searchLine;
+
+        [FindsBy(How = How.Name, Using = "btnK")]
+        private IWebElement searchButton;
+
+        [FindsBy(How = How.Name, Using = "btnI")]
+        private IWebElement luckyButton;
+
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("http://google.com");
+        }
+
+        public void enterTextToSearchLine(string text)
+        {
+            searchLine.Click();
+            searchLine.SendKeys(text);
+        }
+
+        public void clickOnSearchButton()
+        {
+            searchButton.Click();
+        }
+
+        public void clickOnLuckyButton()
+        {
+            luckyButton.Click();
+        }
+
+        public void clickOnEnter()
+        {
+            searchButton.SendKeys(Keys.Enter);
+        }
+
+
+
+
+    }
+}
---
+++ PageObject/TestClass.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObject.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObject
+{
+    public class TestClass
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void GoogleSearch()
+        {
+            GoogleSearchPage homePage = new GoogleSearchPage(driver);
+            homePage.goToPage();
+            string textToSearch = "Wikipedia";
+            homePage.enterTextToSearchLine(textToSearch);
+            homePage.clickOnEnter();
+            Assert.AreEqual("Wikipedia - Поиск в Google", driver.Title);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
